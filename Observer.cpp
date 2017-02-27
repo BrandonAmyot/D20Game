e59f42399c5e5d8906c242202f944f67dfcf465f
Brandon Amyot <amyot.brandon@hotmail.com>
2017-02-27 13:44:01 -0500

    Update Observer.cpp

--- Observer.cpp
+++ Observer.cpp
@@ -1,6 +1,5 @@
 //!@file
 //!@brief Implementation of Character Observer class.
-//!@author Brandon Amyot - 26990940
 //!This .cpp file implements members of character observer class
 #include "Observer.h"
 
